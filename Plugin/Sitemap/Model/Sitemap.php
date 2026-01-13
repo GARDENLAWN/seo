c5562181fdee1bb833ad959d05cad94c2eb07017
GARDENLAWN <rpcsky@gmail.com>
2026-01-13 17:58:46 +0100

    Init

--- Plugin/Sitemap/Model/Sitemap.php
+++ Plugin/Sitemap/Model/Sitemap.php
@@ -5,6 +5,7 @@ namespace GardenLawn\Seo\Plugin\Sitemap\Model;
 use Magento\Framework\Filesystem;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Filesystem\DriverPool;
+use Psr\Log\LoggerInterface;
 
 class Sitemap
 {
@@ -14,12 +15,20 @@ class Sitemap
     private $filesystem;
 
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @param Filesystem $filesystem
+     * @param LoggerInterface $logger
      */
     public function __construct(
-        Filesystem $filesystem
+        Filesystem $filesystem,
+        LoggerInterface $logger
     ) {
         $this->filesystem = $filesystem;
+        $this->logger = $logger;
     }
 
     /**
@@ -31,7 +40,8 @@ class Sitemap
     public function beforeGenerateXml(
         \Magento\Sitemap\Model\Sitemap $subject
     ) {
-        $this->setLocalDirectory($subject);
+        $this->logger->info('GardenLawn_Seo: beforeGenerateXml started');
+        $this->setLocalDirectories($subject);
         return [];
     }
 
@@ -44,29 +54,42 @@ class Sitemap
     public function beforeSave(
         \Magento\Sitemap\Model\Sitemap $subject
     ) {
-        $this->setLocalDirectory($subject);
+        $this->logger->info('GardenLawn_Seo: beforeSave started');
+        $this->setLocalDirectories($subject);
         return [];
     }
 
     /**
-     * Set local directory driver
+     * Set local directory drivers
      *
      * @param \Magento\Sitemap\Model\Sitemap $subject
      */
-    private function setLocalDirectory($subject)
+    private function setLocalDirectories($subject)
     {
-        // Use reflection to set the directory write instance to local file driver
-        // This bypasses S3 or other remote storage drivers configured globally
+        try {
+            // Force _directory to be local PUB
+            $pubDirectory = $this->filesystem->getDirectoryWrite(DirectoryList::PUB, DriverPool::FILE);
 
-        // We need to use DirectoryList::PUB to get the 'pub' directory
-        // But we also need to make sure we are writing to the correct path relative to pub
-        // The original code uses DirectoryList::PUB as the root for _directory
+            // Use the specific class name to access private properties
+            // This is crucial because $subject is an Interceptor and private properties
+            // of the parent class are not visible via reflection on the Interceptor instance/class directly
+            $reflection = new \ReflectionClass(\Magento\Sitemap\Model\Sitemap::class);
 
-        $directory = $this->filesystem->getDirectoryWrite(DirectoryList::PUB, DriverPool::FILE);
+            $directoryProperty = $reflection->getProperty('_directory');
+            $directoryProperty->setAccessible(true);
+            $directoryProperty->setValue($subject, $pubDirectory);
 
-        $reflection = new \ReflectionClass($subject);
-        $property = $reflection->getProperty('_directory');
-        $property->setAccessible(true);
-        $property->setValue($subject, $directory);
+            // Force tmpDirectory to be local SYS_TMP
+            $tmpDirectory = $this->filesystem->getDirectoryWrite(DirectoryList::SYS_TMP, DriverPool::FILE);
+
+            $tmpProperty = $reflection->getProperty('tmpDirectory');
+            $tmpProperty->setAccessible(true);
+            $tmpProperty->setValue($subject, $tmpDirectory);
+
+            $this->logger->info('GardenLawn_Seo: Directories forced to local driver. Pub path: ' . $pubDirectory->getAbsolutePath());
+
+        } catch (\Exception $e) {
+            $this->logger->error('GardenLawn_Seo: Error setting local directories: ' . $e->getMessage());
+        }
     }
 }
